796c8662eab8273f5d49ceb2adbd1661a296d2cb
Michael Klishin <michael@defprotocol.org>
2013-08-12 08:31:12 +0400

    Prepare for being added to travis-ci.org

--- project.clj
+++ project.clj
@@ -3,4 +3,18 @@
   :url "http://github.com/clojurewerkz/persephone"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:plugins      [[codox "0.6.4"]]
+                   :dependencies [[com.novemberain/monger "1.5.0"]
+                                  [cheshire               "5.1.1"]
+                                  [hiccup                 "1.0.3"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.6:+master"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
