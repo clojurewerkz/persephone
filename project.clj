25c9b280c6dcdcd92356a647c3faee1a555403e3
Michael Klishin <michael@clojurewerkz.org>
2014-03-08 14:54:01 +0400

    bump Codox

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:plugins      [[codox "0.6.4"]]
+  :profiles {:dev {:plugins      [[codox 0.6.6]]
                    :dependencies [[com.novemberain/monger "1.5.0"]
                                   [cheshire               "5.1.1"]
                                   [hiccup                 "1.0.3"]]}
